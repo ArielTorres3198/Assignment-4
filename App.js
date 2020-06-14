836b3de62070fa8030716b4f3a7c0dd5032f6414
Jsze5341 <33404576+Jsze5341@users.noreply.github.com>
2020-06-14 19:04:10 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ReactDOM from "react-dom";
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
+//import "./info.css";
+//import App from "./App";
+//import "bootstrap/dist/css/bootstrap.css";
+
+class Info extends React.Component {
+  addRows(){
+    console.log("Add Rows");
+  }
+
+  addColumns(){
+    console.log("Add Columns");
+  }
+
+  removeRows(){
+    console.log("Remove Rows");
+  }
+
+  removeColumns(){
+    console.log("Remove Columns");
+  }
+  render() {
+    const Naviheader = {
+      color: "BlueSapphire",
+      backgroundColor: "LightBlue",
+      padding: "10px",
+      fontFamily: "Monospace"
+    };
+    const Infoheader = {
+      padding: "10px",
+      fontFamily: "Monospace",
+      textAlign: "center"
+    };
+    const Body = {
+      backgroundColor: "LightGray",
+      padding: "10px",
+      fontFamily: "Monospace"
+    };
+
+    return<div>
+      <button onClick={this.addRows}>Add Rows</button>
+      <button onClick={this.addColumns}>Add Columns</button>
+      <button onClick={this.removeRows}>Remove Rows</button>
+      <button onClick={this.removeColumns}>Remove Columns</button>
+    </div>
+  }
+}
+
+export default Info;
+
