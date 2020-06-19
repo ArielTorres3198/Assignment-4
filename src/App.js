441abd7fb94c9933400861f8d954ec4755b2de68
Jsze5341 <33404576+Jsze5341@users.noreply.github.com>
2020-06-18 20:40:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ class Grid extends React.Component {
 
     else
     {
-      temp.pop(<td></td>)
+      temp.pop();
       tempref.pop();
     }
 
@@ -159,7 +159,7 @@ class Grid extends React.Component {
     {  
       for (let i = 0; i < temp.length; i++)
       {
-        temp[i].pop(<td></td>)
+        temp[i].pop();
         tempref[i].pop();
       }
     }
