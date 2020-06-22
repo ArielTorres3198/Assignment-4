01d088a62d4f587164d628ac451f5ceebe759e1a
ArielTorres3198 <44238602+ArielTorres3198@users.noreply.github.com>
2020-06-22 10:19:17 -0400

    Adding dragging

--- src/App.js
+++ src/App.js
@@ -1,59 +1,146 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import ReactDOM from "react-dom";
-import { findRenderedComponentWithType } from 'react-dom/test-utils';
 import "./index.css";
-//import App from "./App";
-//import "bootstrap/dist/css/bootstrap.css";
+
+//Global variable makes it easier to assign 
+//background colors to each cell
+var color = "white";
+
+class Cell extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      bgColor: "",
+	  testBool: false,
+    };
+  }
+
+isPressed(){
+    this.setState({bgColor: color});
+    this.setState({testBool: true});
+	/*if (this.state.testBool !== true)
+	{
+    	alert("Not true");
+	}*/
+ }
+
+ isNotPressed(){
+	this.setState({testBool: false});
+ }
+
+ fillCell(){
+	if (this.state.testBool === true)
+	{
+   		this.setState({bgColor: color});
+	}
+ } 
+
+ fillUncolored()
+ {
+   console.log("Original Color: ", this.state.bgColor);
+   if (this.state.bgColor === "white" || this.state.bgColor === "")
+   {
+     this.fillCell();
+     console.log("New Color: ", this.state.bgColor);
+   }
+ }
+
+  render() {
+    return(
+    <td 
+		style ={{backgroundColor: this.state.bgColor}} 
+		onMouseOver={() => this.fillCell()}
+		onMouseDown={() => this.isPressed()}
+		onMouseUp={() => this.isNotPressed()}
+	></td>
+    );
+  }
+}
 
 class Grid extends React.Component {
   constructor(props){
     super(props);
-	
-    this.state = {
-      table: [[<td></td>,<td></td>],[<td></td>,<td></td>]],
 
+    //Base table
+    this.state = {
+      table: [], //Table array
+      bgColor: "", //Background cell color
+      reflist:[], //Access to class Cell tds 
     };
   }
 
+  //Creates table
   returnTrs(){
     var temp = this.state.table;
     
     var trs = [];
     for(let i = 0; i < temp.length; i++)
-      trs.push(<tr>{temp[i]}</tr>);
+      trs.push(<tr style = {{backgroundColor: "white"}}>{temp[i]}</tr>);
 
     return trs;
+  }
+
+  //Color assigning
+  colorChange(selVal) {
+    color = selVal;
   }
 
   addRows(){
     console.log("Add Rows");
     var temp = this.state.table;
     var row = [];
-    
-    if (temp.length === 0)
-      row.push(<td></td>)
-
-    else
-      row.push(temp[0]);
+    var temprefs = this.state.reflist;
+    var newrefs = [];
+		
+   if (temp.length === 0)
+   {
+      const tempref = React.createRef();
+      row.push(<Cell ref={tempref}/>); 
+      newrefs.push(tempref);
+   }
+   else
+   {
+	 for (let i = 0; i < temp[0].length; i++)
+	 {
+		 const tempref  = React.createRef();
+		 row.push(<Cell ref={tempref}/>);
+		 newrefs.push(tempref);
+	 }
+   }
    
     temp.push(row);
+    temprefs.push(newrefs);
     this.setState({table:temp});
+    this.setState({reflist:temprefs });
   }
 
   addColumns(){
     console.log("Add Columns");
     var temp = this.state.table;
 
+  	var temprefs  = this.state.reflist;
+	  var newrefs = [];
+	
+	
     if (temp.length === 0)
-      temp.push([<td></td>])
-
+    {
+      const tempref  = React.createRef();
+      temp.push([<Cell ref={tempref}/>]);
+      newrefs.push([tempref]);
+      temprefs.push(newrefs);
+    }
     else
     {
       for (let i = 0; i < temp.length; i++)
-	    	temp[i].push(<td></td>);
-    }
+      {
+        const tempref  = React.createRef();
+        temp[i].push(<Cell ref={tempref}/>);
+        temprefs[i].push(tempref);
+      }
 
+    }
+	
+    this.setState({reflist:temprefs});
     this.setState({table:temp});
 
   }
@@ -61,12 +148,16 @@ class Grid extends React.Component {
   removeRows(){
     console.log("Remove Rows");
     var temp = this.state.table;
+    var tempref = this.state.reflist;
 
     if (temp.length === 0)
       alert("No rows to remove");
 
     else
-      temp.pop(<td></td>);
+    {
+      temp.pop();
+      tempref.pop();
+    }
 
     this.setState({table:temp});
   }
@@ -74,6 +165,7 @@ class Grid extends React.Component {
   removeColumns(){
     console.log("Remove Columns");
     var temp = this.state.table;
+    var tempref = this.state.reflist;
 
     if (temp.length === 0)
       alert("No columns to remove");
@@ -81,7 +173,10 @@ class Grid extends React.Component {
     else
     {  
       for (let i = 0; i < temp.length; i++)
-        temp[i].pop(<td></td>);
+      {
+        temp[i].pop();
+        tempref[i].pop();
+      }
     }
 
     this.setState({table:temp});
@@ -89,18 +184,42 @@ class Grid extends React.Component {
 
   fillAllUncolored(){
     console.log("Fill All Uncolored");
+    	
+    var temprefs = this.state.reflist;
+    console.log(temprefs);
+    console.log(this.state.table);
+    for (let i = 0; i < temprefs.length; i++)
+    {
+      for (let j = 0; j < temprefs[i].length; j++)
+      {
+        temprefs[i][j].current.fillUncolored();
+      }
+    }
   }
 
   fillAll(){
     console.log("Fill All");
+	
+    var temprefs = this.state.reflist;
+    console.log(temprefs);
+    console.log(this.state.table);
+    for (let i = 0; i < temprefs.length; i++)
+    {
+      for (let j = 0; j < temprefs[i].length; j++)
+      {
+        temprefs[i][j].current.fillCell();
+      }
+    }
   }
 
   clear(){
     console.log("Clear");
+    var temp = this.state.table;
+    temp = [];
+    this.setState({table:temp});
   }
 
   render() {
-
     return(
 	  <div>
       <button onClick={() => this.addRows()}>Add Rows</button>
@@ -110,12 +229,12 @@ class Grid extends React.Component {
       <button onClick={() => this.fillAllUncolored()}>Fill All Uncolored</button>
       <button onClick={() => this.fillAll()}>Fill All</button>
       <button onClick={() => this.clear()}>Clear</button>
-      <select>
+      <select onChange={(val) => this.colorChange(val.target.value)}>
           <option hidden>Select Color</option>
-          <option>Red</option>
-          <option>Blue</option>
-          <option>Green</option>
-          <option>Yellow</option>
+          <option value = "red">Red</option>
+          <option value = "blue">Blue</option>
+          <option value = "green">Green</option>
+          <option value = "yellow">Yellow</option>
       </select>
       <table>
 	  {this.returnTrs()}
@@ -133,4 +252,3 @@ ReactDOM.render(
 );
 
 export default Grid;
-
